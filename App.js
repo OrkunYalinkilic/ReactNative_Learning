094ac2a75ebd5e39cc23366540bcfef337ddf97e
Orkun Yalınkılıç <orkunyalinkilic69@gmail.com>
2021-02-20 14:45:16 +0300

    Lesson37-Check_ile_Izin_Kontrolu

--- App.js
+++ App.js
@@ -1,16 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
 import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
+  Platform, // hem android hem de ios için izin istemizi sağlar.
   View,
   Text,
   StatusBar,
@@ -24,94 +17,42 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import {PERMISSIONS} from 'react-native-permissions';
+import { check, PERMISSIONS, checkMultiple } from 'react-native-permissions';
+
+export default class App extends React.Component {
+
+  componentDidMount() {
+
+    const CameraPermission = Platform.select({
+      android: PERMISSIONS.ANDROID.CAMERA,
+      ios: PERMISSIONS.IOS.CAMERA
+    });
+
+    const ReceiveSmsPermission = Platform.select({
+      android: PERMISSIONS.ANDROID.RECEIVE_SMS,
+      ios: PERMISSIONS.IOS.RECEIVE_SMS
+    });
+
+    checkMultiple([CameraPermission, ReceiveSmsPermission])
+      .then((status) => {
+        console.log('Camera', status[CameraPermission]);
+        console.log('ReceiveSms', status[ReceiveSmsPermission]);
+      })
 
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+    /*
+    check(Platform.select({
+      android: PERMISSIONS.ANDROID.CAMERA,
+      ios: PERMISSIONS.IOS.CAMERA
+    }))
+      .then((res) => { alert(res); })
+      .catch((e) => alert(e))*/
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
-export default App;
+  }
+
+  render() {
+    return <View></View>
+  }
+
+}
