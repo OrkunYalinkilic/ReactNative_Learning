caf7f2d95e26db9d4674f05894121b3475077a26
Orkun Yalınkılıç <orkunyalinkilic69@gmail.com>
2021-02-21 13:48:03 +0300

    Lesson41_Push_Notification_Kurulum

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
-
 import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  Platform, // hem android hem de ios için izin istemizi sağlar.
   View,
   Text,
   StatusBar,
@@ -17,29 +15,73 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import { check, PERMISSIONS, checkMultiple, request, requestMultiple, openSettings, checkNotifications, requestNotifications } from 'react-native-permissions';
+import PushNotificationIOS from "@react-native-community/push-notification-ios";
+import PushNotification from "react-native-push-notification";
 
 export default class App extends React.Component {
 
-  componentDidMount() {
+  componentDidMount(){
+    PushNotification.localNotification({
 
-    // UNAVAILABLE => CİHAZDA MEVCUT DEĞİL
-    // DENIED => İZİN İSTENMEDİ / REDDEDİLDİ
-    // GRANTED => İZİN VERİLDİ
-    // BLOCKED => İZİN REDDEDİLDİ ARTIK TALEP EDİLEMEZ
+      title:"Bu bir bildirim başlığı", //optional
+      message:"My Notification Message" //required
 
-    requestNotifications(['alert', 'sound']).then(({ status, settings }) => {
-      console.log(status, settings);
     });
-
-    checkNotifications().then(({ status, settings }) => {
-      console.log(status, settings);
-    });
-
   }
 
   render() {
-    return <View></View>
+    return <SafeAreaView><Text>orkunnn</Text></SafeAreaView>
   }
 
 }
+
+// Must be outside of any component LifeCycle (such as `componentDidMount`).
+PushNotification.configure({
+  // (optional) Called when Token is generated (iOS and Android)
+  onRegister: function (token) {
+    console.log("TOKEN:", token);
+  },
+
+  // (required) Called when a remote is received or opened, or local notification is opened
+  onNotification: function (notification) {
+    console.log("NOTIFICATION:", notification);
+
+    // process the notification
+
+    // (required) Called when a remote is received or opened, or local notification is opened
+    notification.finish(PushNotificationIOS.FetchResult.NoData);
+  },
+
+  // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+  onAction: function (notification) {
+    console.log("ACTION:", notification.action);
+    console.log("NOTIFICATION:", notification);
+
+    // process the action
+  },
+
+ /* // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+  onRegistrationError: function(err) {
+    console.error(err.message, err);
+  },*/
+
+  // IOS ONLY (optional): default: all - Permissions to register.
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+
+  // Should the initial notification be popped automatically
+  // default: true
+  popInitialNotification: true,
+
+  /**
+   * (optional) default: true
+   * - Specified if permissions (ios) and token (android and ios) will requested or not,
+   * - if not, you must call PushNotificationsHandler.requestPermissions() later
+   * - if you are not using remote notification or do not have Firebase installed, use this:
+   *     requestPermissions: Platform.OS === 'ios'
+   */
+  requestPermissions: true,
+});
